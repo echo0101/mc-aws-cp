e36f39ad25da3c1512bece6dff01548ca7f72160
prentice.wongvibulsin <prentice.wongvibulsin@workday.com>
2014-10-22 18:23:14 +0100

    add daemon script and updated shutdown script

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# ./shutdown.sh [api key] [stats api url]
 
+# stop the server
+sudo service cloudcraft stop
+
+# send stats back
 cd vanilla-cloudcraft
 ptyhon upload_stats.py $1 $2
 
+# shutdown
 sudo shutdown -h now
