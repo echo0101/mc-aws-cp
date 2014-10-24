26439f165e1d39603bbe3c405f3300efc1292dde
echo0101 <me@prenticew.com>
2014-10-24 12:53:04 +0100

    clean up mojang api

--- minecontrol/mojang.py
+++ minecontrol/mojang.py
@@ -5,19 +5,15 @@ import json
 def get_uuid(username):
   url = 'https://api.mojang.com/profiles/minecraft'
   user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-  data = json.dumps([username])
   headers = { 
       'User-Agent' : user_agent ,
       'Content-Type' : 'application/json'
       }
-  req = urllib2.Request(url, data, headers)
+  req = urllib2.Request(url, json.dumps([username]), headers)
   response = urllib2.urlopen(req)
 
   users = json.load(response)
 
-  if len(users) > 0:
-    return users[0]["id"]
-
-  return None
+  return users[0]["id"] if len(users) > 0 else None
 
 
